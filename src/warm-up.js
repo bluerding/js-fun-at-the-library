2d364a7cf91c0ef7d63efc3bc9d6d6b4caa0d963
Ben Luerding <bluerding@gmail.com>
2022-11-30 16:02:16 -0600

    Initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -12,23 +12,24 @@
 
 // var title = "Harry Potter";
 // var ...
-
+var title = 'The Hobbit'
+var pages = 304
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
-
+pages = 304 / 4
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie = 
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
-
+var sequel = "title:, This Time It's Personal"
+console.log(sequel)
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
