f561b2acbf7ac687213f97fded9f8e6003e69f79
Ben Luerding <bluerding@gmail.com>
2022-12-01 09:13:28 -0600

    Finish createTitle

--- src/book.js
+++ src/book.js
@@ -1,10 +1,21 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return ("The " + title)
 }
+createTitle("Storm's Awakening")
+
+createTitle("Dancing Sushi")
+createTitle("Dragon in the Summer")
+createTitle("Teenage Ghoul")
+
+function buildMainCharacter() {
+
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
