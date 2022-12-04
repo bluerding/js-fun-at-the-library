00b648dcc8fecd86ccb621b3de1ad513c49ca782
Ben Luerding <bluerding@gmail.com>
2022-12-03 20:59:27 -0600

    make progress on shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,26 @@
+function shelfBook(book, shelf) {
+  if (shelf.length >= 3){
+    return shelf.length
+  } else {
+    shelf.unshift(book)
+  } 
+}
 
+function unshelfBook(book, shelf) {
+  for (i = 0; i < shelf.length; i++) 
+  if (shelf[i].title === book) {
+    shelf.splice(i, 1)
+      return shelf
+  }
+}
+  
+function listTitles(){
+  
+}
+   
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
